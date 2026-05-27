e6b83ed0bd4f2af3382d0f4b4278bad378f79e7d
Shravan Jaiswal <shravanjaiswal685@gmail.com>
2026-05-27 23:11:43 +0530

    jwt

--- src/app.js
+++ src/app.js
@@ -4,21 +4,24 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+app.use(cors({                         //middleware that ensure that server mentioned in .env file could only communicate
     origin:process.env.CORS_ORIGIN,
     credentials:true
 
 }))
 
-app.use(express.json({
+app.use(express.json({    //middleware that parse json data only upto 16 kb
     limit:"16kb"
 }))
 
-app.use(express.urlencoded({
-    extended:true,limit:"16kb"
+/*  URL-encoded data is the format browsers use when you submit an HTML form with method="POST" and enctype="application/x-www-form-urlencoded".
+express.urlencoded() converts that string into a JavaScript object stored in req.body.  */
+
+app.use(express.urlencoded({         
+    extended:true,limit:"16kb"                                                                                                      
 }))
 
-app.use(express.static("public"))
+app.use(express.static("public"))   //transform static files in css or html in json format
 
 app.use(cookieParser())
 export { app }
